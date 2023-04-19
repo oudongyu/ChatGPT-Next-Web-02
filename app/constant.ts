f6ad6554475e9f452972bb428673fa58d4fb14af
moojj moojj <799567669@qq.com>
2023-04-19 12:08:35 +0000

    修改constants文件

--- app/constant.ts
+++ app/constant.ts
@@ -1,8 +1,8 @@
 export const OWNER = "ody";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://blog.csdn.net/weixin_38620636`;
-export const ISSUE_URL = `https://blog.csdn.net/weixin_38620636`;
-export const UPDATE_URL = `https://blog.csdn.net/weixin_38620636`;
+export const REPO_URL = `https://github.com/oudongyu/ChatGPT-Next-Web-02`;
+export const ISSUE_URL = `https://github.com/oudongyu/ChatGPT-Next-Web-02`;
+export const UPDATE_URL = `https://github.com/oudongyu/ChatGPT-Next-Web-02`;
 export const FETCH_COMMIT_URL = `https://github.com/oudongyu/ChatGPT-Next-Web-02`;
-export const FETCH_TAG_URL = `https://blog.csdn.net/weixin_38620636`;
+export const FETCH_TAG_URL = `https://github.com/oudongyu/ChatGPT-Next-Web-02`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
